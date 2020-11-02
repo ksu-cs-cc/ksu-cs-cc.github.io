342cb3bd9312ae18ca03e74843b161fa04389859
russfeld <russfeld@ksu.edu>
2020-11-01 21:54:10 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
 git push -u origin master
 
 #ssh russfeld@cslinux.cs.ksu.edu "cd /web/core && git pull"
-ssh russfeld@cslinux.cs.ksu.edu "cd /home/r/russfeld/cc-website-html && git pull && rsync -aP --exclude='.git' /home/r/russfeld/cc-website-html/ /web/core"
+ssh russfeld@cslinux.cs.ksu.edu "cd /home/r/russfeld/cc-website-html && git pull && rsync -aP --exclude='.git' /home/r/russfeld/cc-website-html/ /web/core && chmod 755 -R /web/core/*"
 
 exit 0
