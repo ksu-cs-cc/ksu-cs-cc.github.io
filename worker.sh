7bcd08fa627b00c68d4ef8badfb56dd00b0ebe57
Russell Feldhausen <russfeldh@gmail.com>
2021-03-15 11:40:57 -0500

    Testing new deploy

---
+++ worker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ../cc-website-html
+git pull
+git rm -rf .
+git clean -fxd
+
+cd ../cc-website
+git pull --recurse-submodules
+
+hugo
+cd ../cc-website-html
+git add .
+git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
+git push -u origin master
+
+#ssh russfeld@cslinux.cs.ksu.edu "cd /web/core && git pull"
+ssh russfeld@cslinux.cs.ksu.edu "cd /home/r/russfeld/cc-website-html && git pull && rsync -aP --exclude='.git' /home/r/russfeld/cc-website-html/ /web/core && chmod -R a+rx /web/core/*"
+
+exit 0
