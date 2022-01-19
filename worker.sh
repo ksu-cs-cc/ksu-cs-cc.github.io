dc94bb8540a3acce9f8a46a8728732a01c04f2b7
russfeld <russfeld@ksu.edu>
2022-01-19 10:38:44 -0600

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+LOG_FILE=/home/textbooks/cc-website.log
+DISCORD="discord://910933777826578502/KhKKD2A8GmgmsWwCVLIx6hb0PPsPqlDi4PDB2HrWR3XJiPOAUntOQWK1qWMx-q_Ob-VC"
+
+echo "=========================================="
+echo `date` >> ${LOG_FILE}
+echo "Started Deploying cc-website" >> ${LOG_FILE}
+apprise -vv -b "Started Deploying cc-website" ${DISCORD} >> ${LOG_FILE} 2>&1
+
 cd ../cc-website-html
 git pull
 git rm -rf .
@@ -14,7 +22,15 @@ git add .
 git commit -m "Automatic Deploy on `date +'%Y-%m-%d %H:%M:%S'`"
 git push -u origin master
 
+apprise -vv -b "Started rsync for cc-website" ${DISCORD} >> ${LOG_FILE} 2>&1
+echo "Started rsync for cc-website" >> ${LOG_FILE}
+
 #ssh russfeld@cslinux.cs.ksu.edu "cd /web/core && git pull"
 ssh russfeld@cslinux.cs.ksu.edu "cd /home/r/russfeld/cc-website-html && git pull && rsync -aP --exclude='.git' /home/r/russfeld/cc-website-html/ /web/core && chmod -R a+rx /web/core/*"
+
+apprise -vv -b "Finished Deploying cc-website" ${DISCORD} >> ${LOG_FILE} 2>&1
+echo "Finished Deploying cc-website" >> ${LOG_FILE}
+
+echo "=========================================="
 
 exit 0
